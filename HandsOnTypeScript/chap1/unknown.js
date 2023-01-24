85f0cfa31816bd62d9847d0b7f2f776b7e4a9a5a
PolyakovSergey <ancientcult89@gmail.com>
2023-01-24 19:52:45 +0700

    page 95

---
+++ HandsOnTypeScript/chap1/unknown.js
@@ -0,0 +1,6 @@
+var val1 = 22;
+val1 = "string value";
+val1 = new Array();
+if (val1 instanceof Array)
+    val1.push(33);
+console.log(val1);
---
+++ HandsOnTypeScript/chap1/unknown.ts
@@ -0,0 +1,6 @@
+let val1: unknown = 22;
+val1 = "string value";
+val1 = new Array();
+if(val1 instanceof Array)
+    val1.push(33);
+console.log(val1);
